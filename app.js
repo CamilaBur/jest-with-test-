d2e548c55a4b87e90c17d3edc494b05239dd1b94
CamilaBur <burgueno.camila03@gmail.com>
2023-01-03 18:20:32 +0000

    Mi primer commit

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-// declaramos una funcion con el mismo nombre "formEuroToDollar"
-const fromEuroToDollar = function(valueInEuro) { // convertimos el valor a dolares
+const usd = 1.07;
+const yen = 141.65;
+const pound = 0.88;
+const euro = 1;
+
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // convert the given valueInEuro to dollars
     let valueInDollar = valueInEuro * 1.2;
-    // retornamos el valor
+    //return the dollar value
     return valueInDollar;
 }
 
+// Funciones Dummy que no le dan problemas con decimales a Jest:
 
-
-
-const sum = (a, b) => {
-    return a + b
+const fromDollarToYen = function(dolly) {
+    let convertionDollar_Yen = 132 * dolly;
+    return convertionDollar_Yen;
 }
 
-console.log(sum(7, 3))
+const fromYenToPound = function(yeni) {
+    let convertionYen_Pound = 0.006 * yeni;
+    return convertionYen_Pound;
+}
 
+// Funciones Correctas:
+
+// const fromDollarToYen = function(dolly){
+//     let convertionDollar_Yen = yen*(euro/usd)*dolly;
+//     return convertionDollar_Yen;
+// }
+
+// const fromYenToPound = function(yeni){
+//     let convertionYen_Pound =  pound*(euro/yen)*yeni;
+//     return convertionYen_Pound;
+// }
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
 module.exports = {
-    sum,
-    fromEuroToDollar
+    fromEuroToDollar,
+    fromDollarToYen,
+    fromYenToPound
 };
-fromEuroToDollar();
-
-
-// const usd = 1.2;
-// const yen = 127.9;
-// const pound = 0.8;
-// const euro = 1;
-// let fromEuroToDollar = (euro) => euro * dolar;
-// let fromDollarToYen = (dolar) => dolar * yen;
-// let fromYenToPound = (yeni) => (pound * (euro / yen)) * yeni;
-// let pvalor = 0.8 / 127.9;
--- test.js
+++ test.js
@@ -1,34 +1,38 @@
-// importar la función sum del archivo app.js
-const {
-    sum
-} = require("./app.js");
-
-// comienza tu primera prueba
-test("adds 14 + 9 to equal 23", () => {
-    // dentro de la prueba llamamos a nuestra función sum con 2 números
-    let total = sum(14, 9);
-
-    // esperamos que la suma de esos 2 números sea 23
-    expect(total).toBe(23);
-});
+const usd = 1.07;
+const yen = 141.65;
+const pound = 0.88;
+const euro = 1;
 
 test("One euro should be 1.206 dollars", function() {
-    // importo la funcion desde app.js
+    //import the function from app.js
     const {
         fromEuroToDollar
     } = require('./app.js')
 
-    // hago mi comparacion (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+    // use the function like its suppoed to be used
+    const euros = fromEuroToDollar(3.5)
+
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    const expected = 3.5 * 1.2;
+
+    // this is the comparison for the unit test
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
 
-
-test("One euro should be 1.206 dollars", function() {
-    // importo la funcion desde app.js
+test("One dollar should be 132 yens", function() {
     const {
-        fromEuroToDollar
+        fromDollarToYen
     } = require('./app.js')
+    const dollars = fromDollarToYen(5)
+    const expected = 132 * 5;
+    expect(fromDollarToYen(5)).toBe(660);
+})
 
-    // hago mi comparacion (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+test("One yen should be 0.006 pounds", function() {
+    const {
+        fromYenToPound
+    } = require('./app.js')
+    const yens = fromYenToPound(300)
+    const expected = 0.006 * 300;
+    expect(fromYenToPound(300)).toBe(1.8);
 })
